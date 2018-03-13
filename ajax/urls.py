2e41784a79f84c7b44f42d6a09007bfbec188ec8
Mustafa Kemal Dirken <mkdirken@gmail.com>
2018-03-13 17:59:22 +0300

    profil ayarları kısmı yapıldı

--- ajax/urls.py
+++ ajax/urls.py
@@ -2,5 +2,8 @@ from django.urls import path,include
 from .views import *
 app_name="ajax"
 urlpatterns = [
-   path('login',login,name='login')
+   path('login',login,name='login'),
+   path('update_user',update_user,name='update_user'),
+   path('update_password',update_password,name='update_password'),
+   path('update_avatar',update_avatar,name='update_avatar')
 ]
